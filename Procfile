4d56cc3f3f545fa4211f6a42e9ba21bf9f14ee1d
S <skyq@sky.local>
2022-05-01 13:23:02 +0530

    fix

--- Procfile
+++ Procfile
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 2551ff31f76b314cf86811e3788a93e2b96fbe88
 web: gunicorn wsgi:app
--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
 from app.main import app
 if __name__ == "__main__":
   app.run()
-=======
-
-from app.main import app
- 
-if __name__ == "__main__":
-        app.run()
->>>>>>> 2551ff31f76b314cf86811e3788a93e2b96fbe88
